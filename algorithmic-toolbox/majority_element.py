3a5df44e6fb69cfe190721a0fcd096e181b799d4
vikassry <vikassry@gmail.com>
2017-06-08 17:55:48 +0530

    Majority element passed

--- algorithmic-toolbox/majority_element.py
+++ algorithmic-toolbox/majority_element.py
@@ -1,16 +1,18 @@
 # Uses python3
 import sys
 
+# return 1 if a number occurs more than n/2 times in given list or returns -1 
+
 def get_majority_element(a, left, right):
     if left == right:
         return -1
     number_count = {}
-    for i in range(left+1, right):        
-        number_count[a[i]] = number_count[a[i]] + 1 if number_count.get(a[i])!=None else 1
-
-    print(number_count)
+    
+    for i in range(left, right):        
+        number_count[a[i]] = number_count[a[i]]+1 if number_count.get(a[i]) != None else 1
+    
     for count in number_count.values():
-        if count > 1:
+        if count > right/2:
             return 1
     return -1
 
@@ -21,4 +23,3 @@ if __name__ == '__main__':
         print(1)
     else:
         print(0)
-    
