9c8520f7f0693fe5a04d4df29f4ae4a04ee7efcb
vikassry <vikassry@gmail.com>
2017-06-12 02:47:57 +0530

    Fixed knapsack without repeatetion

--- algorithmic-toolbox/knapsack.py
+++ algorithmic-toolbox/knapsack.py
@@ -1,15 +1,21 @@
 # Uses python3
 import sys
 
-def optimal_weight(W, w):
-    # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+def knapsack(W, weights):
+	n = len(weights)
+	values = [[0 for x in range(W+1)] for x in range(n+1)]
+
+	for i in range(n+1):
+		for w in range(W+1):
+			if i==0 or w==0:
+				values[i][w] = 0
+			elif weights[i-1] <= w:
+				values[i][w] = max(weights[i-1] + values[i-1][w-weights[i-1]],  values[i-1][w])
+			else:
+				values[i][w] = values[i-1][w]
+	return values[n][W]
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    W, n, *w = list(map(int, input.split()))
-    print(optimal_weight(W, w))
+	input = sys.stdin.read()
+	W, n, *weights = list(map(int, input.split()))
+	print(knapsack(W, weights))
